6bda1062601b01efb7f720f4f58ec20115aef0d5
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-02-14 16:50:31 +0200

    Pass the props through a destructuring assignment.

--- learning_react/react_complete_guide/01-starting-project/src/App.jsx
+++ learning_react/react_complete_guide/01-starting-project/src/App.jsx
@@ -26,7 +26,7 @@ function Header() {
 function CoreConcept(props) {
   return (
     <li>
-      <img src={props.img} alt={props.title} />
+      <img src={props.image} alt={props.title} />
       <h3>{props.title}</h3>
       <p>{props.description}</p>
     </li>
@@ -41,25 +41,13 @@ function App() {
         <section id="core-concepts">
           <h2>Core concepts</h2>
           <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              img={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[1].title}
-              description={CORE_CONCEPTS[1].description}
-              img={CORE_CONCEPTS[1].image}
-            />{" "}
-            <CoreConcept
-              title={CORE_CONCEPTS[2].title}
-              description={CORE_CONCEPTS[2].description}
-              img={CORE_CONCEPTS[2].image}
-            />{" "}
+            <CoreConcept {...CORE_CONCEPTS[0]}></CoreConcept>
+            <CoreConcept {...CORE_CONCEPTS[1]}></CoreConcept>
+            <CoreConcept {...CORE_CONCEPTS[2]}></CoreConcept>
             <CoreConcept
               title={CORE_CONCEPTS[3].title}
               description={CORE_CONCEPTS[3].description}
-              img={CORE_CONCEPTS[3].image}
+              image={CORE_CONCEPTS[3].image}
             />
           </ul>
         </section>
