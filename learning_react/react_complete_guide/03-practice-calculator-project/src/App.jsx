0ec10d098eb95ed9f56634b1a893f45ff87d25e2
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-02-20 12:25:32 +0200

    Add user inputs.

--- learning_react/react_complete_guide/03-practice-calculator-project/src/App.jsx
+++ learning_react/react_complete_guide/03-practice-calculator-project/src/App.jsx
@@ -7,6 +7,24 @@ function App() {
         <img src="/investment-calculator-logo.png" alt="" />
         <h1>Investment Calculator</h1>
       </div>
+      <div id="user-input">
+        <div class="input-group">
+          <div>
+            <label htmlFor="initialInvestment">INITIAL INVESTMENT</label>
+            <input type="text" id="initialInvestment" />
+
+            <label htmlFor="annualInvestment">ANNUAL INVESTMENT</label>
+            <input type="text" id="annualInvestment" />
+          </div>
+          <div>
+            <label htmlFor="expectedReturn">EXPECTED RETURN</label>
+            <input type="text" id="expectedReturn" />
+
+            <label htmlFor="duration">DURATION</label>
+            <input type="number" id="duration" />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
