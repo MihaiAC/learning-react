96e202da9eac0af63f0e3c7206bb388e8e406c4b
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-02-20 11:12:12 +0200

    Init practice React project.

---
+++ learning_react/react_complete_guide/03-practice-calculator-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { calculateInvestmentResults, formatter } from "./util/investment";
+
+function App() {
+  return (
+    <>
+      <div id="header">
+        <img src="/investment-calculator-logo.png" alt="" />
+        <h1>Investment Calculator</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
