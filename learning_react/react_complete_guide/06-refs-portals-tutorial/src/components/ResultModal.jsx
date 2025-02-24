386f446c866637d0b55ea349bea556dff242d823
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-02-24 18:01:19 +0200

    Handle game outcome based on remaining time.

--- learning_react/react_complete_guide/06-refs-portals-tutorial/src/components/ResultModal.jsx
+++ learning_react/react_complete_guide/06-refs-portals-tutorial/src/components/ResultModal.jsx
@@ -1,8 +1,15 @@
 import { useImperativeHandle, useRef } from "react";
 
 /* eslint-disable react/prop-types */
-export default function ResultModal({ ref, result, targetTime }) {
+export default function ResultModal({
+  ref,
+  targetTime,
+  remainingTime,
+  onReset,
+}) {
   const dialog = useRef();
+  const userLost = remainingTime < 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
   useImperativeHandle(ref, () => {
     return {
@@ -14,14 +21,15 @@ export default function ResultModal({ ref, result, targetTime }) {
 
   return (
     <dialog className="result-modal" ref={dialog}>
-      <h2>You {result}</h2>
+      {userLost && <h2>You lost</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form action="" method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
