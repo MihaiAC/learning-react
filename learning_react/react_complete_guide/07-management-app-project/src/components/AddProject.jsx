229a76819f82c8adb2686008da872d515048e812
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-02-26 12:12:01 +0200

    Display error modal on submitting empty field.

--- learning_react/react_complete_guide/07-management-app-project/src/components/AddProject.jsx
+++ learning_react/react_complete_guide/07-management-app-project/src/components/AddProject.jsx
@@ -1,16 +1,27 @@
 import { useRef } from "react";
 import StyledInput from "./StyledInput";
+import Modal from "./Modal";
 import PropTypes from "prop-types";
 
 export default function AddProject({ onProjectAdd, onCancel }) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const date = useRef();
 
   function flushInputs() {
-    title.current.value = "";
-    description.current.value = "";
-    date.current.value = "";
+    if (title.current !== undefined) {
+      title.current.value = "";
+    }
+
+    if (description.current !== undefined) {
+      description.current.value = "";
+    }
+
+    if (date.current !== undefined) {
+      date.current.value = "";
+    }
   }
 
   flushInputs();
@@ -21,8 +32,13 @@ export default function AddProject({ onProjectAdd, onCancel }) {
     const enteredDate = date.current.value;
 
     // Validation.
-    if (!title.current.value.trim()) {
-      alert("Title cannot be empty!");
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription === "" ||
+      enteredDate.trim() === ""
+    ) {
+      // Show error modal.
+      modal.current.open();
       return;
     }
 
@@ -38,54 +54,60 @@ export default function AddProject({ onProjectAdd, onCancel }) {
   }
 
   return (
-    <div className="mt-36 w-full">
-      <div className="flex flex-col max-w-lg space-y-4 mt-16 mx-auto">
-        <div className="flex justify-between mb-16">
-          <h1 className="text-4xl font-bold">Add project</h1>
-          <div className="flex space-x-4">
-            <button
-              type="button"
-              onClick={handleSubmit}
-              className="hover:bg-black hover:text-white rounded-md p-2 border-black border-2"
-            >
-              Submit
-            </button>
-            <button
-              type="button"
-              onClick={handleCancel}
-              className="hover:bg-black hover:text-white rounded-md p-2 border-black border-2"
-            >
-              Cancel
-            </button>
+    <>
+      <Modal ref={modal}>
+        <h2>Invalid input</h2>
+        <p>Please make sure that no input fields are empty.</p>
+      </Modal>
+      <div className="mt-36 w-full">
+        <div className="flex flex-col max-w-lg space-y-4 mt-16 mx-auto">
+          <div className="flex justify-between mb-16">
+            <h1 className="text-4xl font-bold">Add project</h1>
+            <div className="flex space-x-4">
+              <button
+                type="button"
+                onClick={handleSubmit}
+                className="hover:bg-black hover:text-white rounded-md p-2 border-black border-2"
+              >
+                Submit
+              </button>
+              <button
+                type="button"
+                onClick={handleCancel}
+                className="hover:bg-black hover:text-white rounded-md p-2 border-black border-2"
+              >
+                Cancel
+              </button>
+            </div>
           </div>
+
+          <StyledInput
+            id="add-project-form-title"
+            label="Title"
+            type="text"
+            //value={title.current}
+            ref={title}
+          />
+
+          <StyledInput
+            id="add-project-form-descr"
+            label="Description"
+            type="textarea"
+            //value={description}
+            ref={description}
+          />
+
+          <StyledInput
+            id="add-project-form-date"
+            label="Due date"
+            type="date"
+            //value={date}
+            ref={date}
+            //onChange={(e) => setDate(e.target.value)}
+          />
         </div>
-
-        <StyledInput
-          id="add-project-form-title"
-          label="Title"
-          type="text"
-          //value={title.current}
-          ref={title}
-        />
-
-        <StyledInput
-          id="add-project-form-descr"
-          label="Description"
-          type="textarea"
-          //value={description}
-          ref={description}
-        />
-
-        <StyledInput
-          id="add-project-form-date"
-          label="Due date"
-          type="date"
-          //value={date}
-          ref={date}
-          //onChange={(e) => setDate(e.target.value)}
-        />
       </div>
-    </div>
+    </>
   );
 }
 
