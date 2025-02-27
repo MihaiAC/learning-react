e642c81ce337f6a1f7dbbb1d761c3eddadde676e
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-02-27 13:00:54 +0200

    Shuffle the displayed answers and check when quiz is finished.

--- learning_react/react_complete_guide/10-quiz-app-project/src/components/Quiz.jsx
+++ learning_react/react_complete_guide/10-quiz-app-project/src/components/Quiz.jsx
@@ -1,22 +1,34 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
-
-  // 2 answers => we're at question 3 (index 2).
   const activeQuestionIdx = userAnswers.length;
+  const isQuizFinished = activeQuestionIdx === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
   }
+
+  if (isQuizFinished) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz finished!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIdx].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIdx].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIdx].answers.map((answer, idx) => (
+          {shuffledAnswers.map((answer, idx) => (
             <li key={idx} className="answer">
               <button type="button" onClick={() => handleSelectAnswer(answer)}>
                 {answer}
