8e65cb3ae7d6636be7b67fde24a46fd3823b395e
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-02-27 12:49:46 +0200

    Add Quiz component first pass.

--- learning_react/react_complete_guide/10-quiz-app-project/src/components/Quiz.jsx
+++ learning_react/react_complete_guide/10-quiz-app-project/src/components/Quiz.jsx
@@ -1,1 +1,30 @@
+import { useState } from "react";
+import QUESTIONS from "../questions.js";
 
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  // 2 answers => we're at question 3 (index 2).
+  const activeQuestionIdx = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIdx].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIdx].answers.map((answer, idx) => (
+            <li key={idx} className="answer">
+              <button type="button" onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
