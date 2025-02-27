98effaa1e166655e3ea305c89228ee452f9c0243
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-02-27 23:58:45 +0200

    Replace useEffect with key.

--- learning_react/react_complete_guide/11-react-behind-the-scenes/src/components/Counter/Counter.jsx
+++ learning_react/react_complete_guide/11-react-behind-the-scenes/src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState, memo, useCallback, useMemo, useEffect } from "react";
+import { useState, memo, useCallback, useMemo } from "react";
 
 import IconButton from "../UI/IconButton.jsx";
 import MinusIcon from "../UI/Icons/MinusIcon.jsx";
@@ -38,9 +38,9 @@ const Counter = memo(function Counter({ initialCount }) {
     { value: initialCount, id: Math.random() * 1000 },
   ]);
 
-  useEffect(() => {
-    setCounterChanges([{ value: initialCount, id: Math.random() * 1000 }]);
-  }, [initialCount]);
+  // useEffect(() => {
+  //   setCounterChanges([{ value: initialCount, id: Math.random() * 1000 }]);
+  // }, [initialCount]);
 
   const currentCounter = counterChanges.reduce(
     (prevCounter, counterChange) => prevCounter + counterChange.value,
