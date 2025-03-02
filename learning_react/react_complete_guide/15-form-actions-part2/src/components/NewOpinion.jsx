d5614d99ae98f6648c22148d961a060dd3cdd2fc
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-03-02 22:00:33 +0200

    Send form data to the "backend".

--- learning_react/react_complete_guide/15-form-actions-part2/src/components/NewOpinion.jsx
+++ learning_react/react_complete_guide/15-form-actions-part2/src/components/NewOpinion.jsx
@@ -1,7 +1,10 @@
-import { useActionState } from "react";
+import { useActionState, use } from "react";
+import { OpinionsContext } from "../store/opinions-context";
 
 export function NewOpinion() {
-  function shareOpinionAction(prevFormState, formData) {
+  const { addOpinion } = use(OpinionsContext);
+
+  async function shareOpinionAction(prevFormState, formData) {
     const title = formData.get("title");
     const body = formData.get("body");
     const userName = formData.get("userName");
@@ -27,6 +30,7 @@ export function NewOpinion() {
       };
     }
 
+    await addOpinion({ title, body, userName });
     // Clear the form.
     return { errors: null };
   }
