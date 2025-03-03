a6641475c2186fc6a7d700afb520d88f9424fb51
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-03-03 20:07:01 +0200

    Remove tight coupling in ModalContext.

--- learning_react/react_complete_guide/16-food-order-project/src/components/ModalContext.jsx
+++ learning_react/react_complete_guide/16-food-order-project/src/components/ModalContext.jsx
@@ -3,39 +3,26 @@ import { createContext, useCallback, useState } from "react";
 
 const initialState = {
   activeModal: "", // Values: "" / "cart" / "checkout"
-  openCart: () => {},
-  openCheckout: () => {},
-  closeActiveModal: () => {},
+  openModal: () => {},
+  closeModal: () => {},
 };
-
-// Tight coupling between ModalContext and what components
-// use the modals.
-const ModalTypes = Object.freeze({
-  CART: "cart",
-  CHECKOUT: "checkout",
-  NONE: "",
-});
 
 export const ModalContext = createContext(initialState);
 
 export function ModalContextProvider({ children }) {
   const [activeModal, setActiveModal] = useState("");
 
-  const openCart = useCallback(() => {
-    setActiveModal(ModalTypes.CART);
-  }, []);
-
-  const openCheckout = useCallback(() => {
-    setActiveModal(ModalTypes.CHECKOUT);
-  }, []);
-
-  const closeActiveModal = useCallback(() => {
+  const closeModal = useCallback(() => {
     setActiveModal("");
   }, []);
 
   return (
     <ModalContext.Provider
-      value={{ activeModal, openCart, openCheckout, closeActiveModal }}
+      value={{
+        activeModal,
+        openModal: setActiveModal,
+        closeModal,
+      }}
     >
       {children}
     </ModalContext.Provider>
