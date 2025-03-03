b71986b5bbb19d03fc9094d37a6646b44583acdc
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-03-03 20:55:48 +0200

    Disable strict mode (adds items twice).

--- learning_react/react_complete_guide/16-food-order-project/src/main.jsx
+++ learning_react/react_complete_guide/16-food-order-project/src/main.jsx
@@ -7,11 +7,9 @@ import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <CartContextProvider>
-      <ModalContextProvider>
-        <App />
-      </ModalContextProvider>
-    </CartContextProvider>
-  </React.StrictMode>
+  <CartContextProvider>
+    <ModalContextProvider>
+      <App />
+    </ModalContextProvider>
+  </CartContextProvider>
 );
