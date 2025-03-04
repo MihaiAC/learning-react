5c1bb1db057aad9305698b1c1d1b4cb14ddd111d
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-03-04 23:15:22 +0200

    Add status notifications for sending data.

--- learning_react/react_complete_guide/18-advanced-redux-tutorial/src/store/displaySlice.js
+++ learning_react/react_complete_guide/18-advanced-redux-tutorial/src/store/displaySlice.js
@@ -2,13 +2,20 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const displaySlice = createSlice({
   name: "display",
-  initialState: { displayCart: false },
+  initialState: { displayCart: false, notification: null },
   reducers: {
     toggleCart(state) {
       state.displayCart = !state.displayCart;
     },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
   },
 });
 
-export const { toggleCart } = displaySlice.actions;
+export const { toggleCart, showNotification } = displaySlice.actions;
 export default displaySlice.reducer;
