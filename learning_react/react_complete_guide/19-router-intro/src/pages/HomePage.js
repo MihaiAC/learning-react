d66e05c276a0298e43974a1ed5641e440552a0ea
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-03-05 14:20:06 +0200

    Add link to Products with useNavigate.

--- learning_react/react_complete_guide/19-router-intro/src/pages/HomePage.js
+++ learning_react/react_complete_guide/19-router-intro/src/pages/HomePage.js
@@ -1,11 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate("/products");
+  }
+
   return (
     <>
       <h1>Home page</h1>
       <p>
         Go to <Link to="/products">products</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Take me to Products</button>
       </p>
     </>
   );
