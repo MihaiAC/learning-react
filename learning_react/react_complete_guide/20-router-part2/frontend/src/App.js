60bc7f67a7fd6a524d4f1a568d8952d4c31923ad
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-03-05 16:31:41 +0200

    Add MainNavigation component.

--- learning_react/react_complete_guide/20-router-part2/frontend/src/App.js
+++ learning_react/react_complete_guide/20-router-part2/frontend/src/App.js
@@ -1,7 +1,4 @@
 // Challenge / Exercise
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// 4. Add properly working links to the MainNavigation
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
 // 6. Output a list of dummy events to the EventsPage
 //    Every list item should include a link to the respective EventDetailPage
 // 7. Output the ID of the selected event on the EventDetailPage
@@ -13,13 +10,20 @@ import EventsPage from "./components/EventsPage";
 import EventDetailPage from "./components/EventDetailPage";
 import NewEventPage from "./components/NewEventPage";
 import EditEventPage from "./components/EditEventPage";
+import MainNavigation from "./components/MainNavigation";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage></HomePage> },
-  { path: "/events", element: <EventsPage /> },
-  { path: "/events/:eventId", element: <EventDetailPage /> },
-  { path: "/events/new", element: <NewEventPage /> },
-  { path: "/events/:eventId/edit", element: <EditEventPage /> },
+  {
+    path: "/",
+    element: <MainNavigation />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/events", element: <EventsPage /> },
+      { path: "/events/:eventId", element: <EventDetailPage /> },
+      { path: "/events/new", element: <NewEventPage /> },
+      { path: "/events/:eventId/edit", element: <EditEventPage /> },
+    ],
+  },
 ]);
 
 function App() {
