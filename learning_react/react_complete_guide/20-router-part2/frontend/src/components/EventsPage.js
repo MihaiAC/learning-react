a2316dae805b51c8490c548c6cb688d96e05724d
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-03-05 16:37:19 +0200

    Add dummy Events and routes to their EventDetail pages.

--- learning_react/react_complete_guide/20-router-part2/frontend/src/components/EventsPage.js
+++ learning_react/react_complete_guide/20-router-part2/frontend/src/components/EventsPage.js
@@ -1,5 +1,23 @@
 import classes from "./EventsPage.module.css";
+import { Link } from "react-router-dom";
+
+const EVENTS = [
+  { id: "p1", title: "Event 1" },
+  { id: "p2", title: "Event 2" },
+  { id: "p3", title: "Event 3" },
+];
 
 export default function EventsPage() {
-  return <h1>Events Page!</h1>;
+  return (
+    <>
+      <h1>Events page</h1>
+      <ul>
+        {EVENTS.map((event) => (
+          <li key={event.id}>
+            <Link to={`${event.id}`}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
