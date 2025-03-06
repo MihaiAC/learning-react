c28e04d302022ea7eae65127e2a896b15eadf979
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-03-06 21:02:59 +0200

    Add simple user creation.

--- learning_react/react_complete_guide/21-authentication/frontend/src/pages/Authentication.js
+++ learning_react/react_complete_guide/21-authentication/frontend/src/pages/Authentication.js
@@ -1,7 +1,50 @@
-import AuthForm from '../components/AuthForm';
+import AuthForm from "../components/AuthForm";
+import { redirect } from "react-router-dom";
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export async function action({ request }) {
+  // Get the mode.
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
+
+  // Get submitted login details.
+  const data = await request.formData();
+  // Name in get matches name in form input field.
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+
+  // Send the request to the backend.
+  const response = await fetch("http://localhost:8080/" + mode, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(authData),
+  });
+
+  // Handle specific error codes.
+  // 422 = unprocessable entity; validation failed or invalid/missing
+  // data in request body
+  // 401 = unauthorized, classic;
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+
+  // Handle all the other error codes.
+  if (!response.ok) {
+    throw Response(
+      JSON.stringify({ message: "Could not authenticate user." }),
+      { status: 500 }
+    );
+  }
+
+  // TODO: manage returned token.
+  return redirect("/");
+}
