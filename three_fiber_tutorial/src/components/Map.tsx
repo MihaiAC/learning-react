1c0bf66cc3349021fd93086c1cb1b9d9d780a7a2
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-03-08 14:44:14 +0200

    Add some grass rows before the starting position.

--- three_fiber_tutorial/src/components/Map.tsx
+++ three_fiber_tutorial/src/components/Map.tsx
@@ -6,6 +6,12 @@ import Row from "./Row";
 export function Map() {
   return (
     <>
+      <Grass rowIndex={-6} />
+      <Grass rowIndex={-5} />
+      <Grass rowIndex={-4} />
+      <Grass rowIndex={-3} />
+      <Grass rowIndex={-2} />
+      <Grass rowIndex={-1} />
       <Grass rowIndex={0} />
       {rows.map((rowData, index) => (
         <Row key={index} rowIndex={index + 1} rowData={rowData} />
