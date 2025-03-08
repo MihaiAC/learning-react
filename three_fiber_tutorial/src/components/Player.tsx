8fbf62e900becf97371da5bf37b8959dabe45924
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-03-08 17:15:23 +0200

    Add player movement animation.

--- three_fiber_tutorial/src/components/Player.tsx
+++ three_fiber_tutorial/src/components/Player.tsx
@@ -1,12 +1,25 @@
 import { Bounds } from "@react-three/drei";
+import { useRef } from "react";
+import usePlayerAnimation from "../hooks/usePlayerAnimation";
+import { Group } from "three";
 
 export function Player() {
+  const player = useRef<Group>(null);
+  usePlayerAnimation(player);
+
+  // Second mesh is the player cap.
   return (
     <Bounds fit clip observe margin={10}>
-      <mesh position={[0, 0, 10]} castShadow receiveShadow>
-        <boxGeometry args={[15, 15, 20]} />
-        <meshLambertMaterial color={0xff7900} flatShading />
-      </mesh>
+      <group ref={player}>
+        <mesh position={[0, 0, 10]} castShadow receiveShadow>
+          <boxGeometry args={[15, 15, 20]} />
+          <meshLambertMaterial color={0xff7900} flatShading />
+        </mesh>
+        <mesh position={[0, 0, 21]} castShadow receiveShadow>
+          <boxGeometry args={[2, 4, 2]} />
+          <meshLambertMaterial color={0xf0619a} flatShading />
+        </mesh>
+      </group>
     </Bounds>
   );
 }
---
+++ three_fiber_tutorial/src/hooks/usePlayerAnimation.ts
@@ -0,0 +1,104 @@
+import { useFrame } from "@react-three/fiber";
+import { stepCompleted, state } from "../stores/player";
+import { tileSize } from "../constants";
+import { Group, Clock, MathUtils } from "three";
+
+export default function usePlayerAnimation(ref: React.RefObject<Group | null>) {
+  const moveClock = new Clock(false);
+
+  useFrame(() => {
+    // When is this undefined?
+    // When player is dead?
+    if (!ref.current) {
+      return;
+    }
+
+    // No moves queued up.
+    if (!state.movesQueue.length) {
+      return;
+    }
+
+    // Get player component.
+    const player = ref.current;
+
+    // Start the clock.
+    if (!moveClock.running) {
+      moveClock.start();
+    }
+
+    // Time(s) to take a step.
+    const stepTime = 0.2;
+
+    // Value between 0 and 1.
+    const progress = Math.min(1, moveClock.getElapsedTime() / stepTime);
+
+    setPosition(player, progress);
+    setRotation(player, progress);
+
+    // Player move ended.
+    if (progress >= 1) {
+      stepCompleted();
+      moveClock.stop();
+    }
+  });
+}
+
+// Calculate start -> end positions based on current tile +
+// direction of move command.
+// Interpolate to current position based on progress.
+// Oz movement - sin
+// Ox Oy movement - line
+function setPosition(player: Group, progress: number) {
+  const startX = state.currentTile * tileSize;
+  const startY = state.currentRow * tileSize;
+
+  let endX = startX;
+  let endY = startY;
+
+  switch (state.movesQueue[0]) {
+    case "left":
+      endX -= tileSize;
+      break;
+    case "right":
+      endX += tileSize;
+      break;
+    case "forward":
+      endY += tileSize;
+      break;
+    case "backward":
+      endY -= tileSize;
+      break;
+    default: {
+      console.log("Invalid move in queue: " + state.movesQueue[0]);
+      return;
+    }
+  }
+
+  player.position.x = MathUtils.lerp(startX, endX, progress);
+  player.position.y = MathUtils.lerp(startY, endY, progress);
+  player.position.z = Math.sin(progress * Math.PI) * 8;
+}
+
+// Player must turn in the direction of the move command.
+function setRotation(player: Group, progress: number) {
+  let endRotation = 0;
+  switch (state.movesQueue[0]) {
+    case "forward":
+      endRotation = 0;
+      break;
+    case "left":
+      endRotation = Math.PI / 2;
+      break;
+    case "right":
+      endRotation = -Math.PI / 2;
+      break;
+    case "backward":
+      endRotation = Math.PI;
+      break;
+    default:
+      console.log("Invalid move in queue: " + state.movesQueue[0]);
+      return;
+  }
+
+  player.rotation.z = MathUtils.lerp(player.rotation.z, endRotation, progress);
+}
