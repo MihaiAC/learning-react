32ad2511c1405e7c27b9765b043db4130b52e173
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-03-17 22:23:00 +0200

    Test SummaryForm initial state.

---
+++ learning_react/testing_react_vitest/sundaes-on-demand/src/components/test/SummaryForm.test.tsx
@@ -0,0 +1,14 @@
+import SummaryForm from "../SummaryForm";
+import { render, screen } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+
+test("Initial conditions", () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  expect(checkbox).not.toBeChecked();
+
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+  expect(confirmButton).toBeDisabled();
+});
