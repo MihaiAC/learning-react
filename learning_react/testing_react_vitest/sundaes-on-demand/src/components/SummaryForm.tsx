ac20a7e3f00206943d3519a7c372ecd03e786e3c
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-03-18 14:09:55 +0200

    Create a simple Popover component that passes the test.

--- learning_react/testing_react_vitest/sundaes-on-demand/src/components/SummaryForm.tsx
+++ learning_react/testing_react_vitest/sundaes-on-demand/src/components/SummaryForm.tsx
@@ -1,20 +1,30 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import Popover from "./ui/Popover";
 
 export default function SummaryForm() {
   const [isChecked, setIsChecked] = useState(false);
+  const formRef = useRef<null | HTMLFormElement>(null);
 
   function handleCheckboxChange() {
     setIsChecked((state) => !state);
   }
 
   return (
-    <div className="flex flex-col space-y-4">
-      <label htmlFor="confirm-order-checkbox">Terms and conditions</label>
-      <input
-        type="checkbox"
-        id="confirm-order-checkbox"
-        onChange={handleCheckboxChange}
-      />
+    <div className="flex-vert">
+      <form className="flex-vert" ref={formRef}>
+        {" "}
+        <label htmlFor="confirm-order-checkbox">Terms and conditions</label>
+        <input
+          type="checkbox"
+          id="confirm-order-checkbox"
+          onChange={handleCheckboxChange}
+        />
+      </form>
+
+      <Popover targetRef={formRef}>
+        <p>Nothing will be actually delivered!</p>
+      </Popover>
+
       <div>
         <button
           disabled={!isChecked}
---
+++ learning_react/testing_react_vitest/sundaes-on-demand/src/components/ui/Popover.tsx
@@ -0,0 +1,39 @@
+import { ReactNode, useEffect, useState } from "react";
+
+interface PopoverProps {
+  children: ReactNode;
+  targetRef: React.RefObject<HTMLFormElement | null>;
+}
+
+export default function Popover({ children, targetRef }: PopoverProps) {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const targetElement = targetRef.current;
+
+    if (!targetElement) {
+      return;
+    }
+
+    const handleMouseEnter = () => setOpen(true);
+    const handleMouseLeave = () => setOpen(false);
+
+    targetElement.addEventListener("mouseenter", handleMouseEnter);
+    targetElement.addEventListener("mouseleave", handleMouseLeave);
+
+    return () => {
+      targetElement.removeEventListener("mouseenter", handleMouseEnter);
+      targetElement.removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, [targetRef]);
+
+  return (
+    <div className="relative">
+      {open && (
+        <div className="absolute left-1/2 top-full mt-2 -translate-x-1/2 w-48 bg-white border shadow-lg rounded-md p-3">
+          {children}
+        </div>
+      )}
+    </div>
+  );
+}
