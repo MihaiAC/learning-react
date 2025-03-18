67b19ebfa1477317750e21ca70e0ba741fd5543f
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-03-18 11:09:56 +0200

    Test that button is disabled after checkbox click.

--- learning_react/testing_react_vitest/sundaes-on-demand/src/components/SummaryForm.tsx
+++ learning_react/testing_react_vitest/sundaes-on-demand/src/components/SummaryForm.tsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
+
 export default function SummaryForm() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  function handleCheckboxChange() {
+    setIsChecked((state) => !state);
+  }
+
   return (
     <div className="flex flex-col space-y-4">
-      <div className="flex flex-col-reverse space-y-4">
-        <input type="checkbox" id="confirm-order-checkbox" />
-        <label htmlFor="confirm-order-checkbox">Terms and conditions</label>
-      </div>
+      <label htmlFor="confirm-order-checkbox">Terms and conditions</label>
+      <input
+        type="checkbox"
+        id="confirm-order-checkbox"
+        onChange={handleCheckboxChange}
+      />
       <div>
-        <button disabled className="bg-red-400 p-2 rounded-md hover:opacity-90">
+        <button
+          disabled={!isChecked}
+          className="bg-red-400 disabled:bg-gray-400 p-2 rounded-md hover:opacity-90"
+        >
           Confirm order
         </button>
       </div>
--- learning_react/testing_react_vitest/sundaes-on-demand/src/components/test/SummaryForm.test.tsx
+++ learning_react/testing_react_vitest/sundaes-on-demand/src/components/test/SummaryForm.test.tsx
@@ -1,6 +1,6 @@
 import SummaryForm from "../SummaryForm";
 import { render, screen } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 
 test("Initial conditions", () => {
   render(<SummaryForm />);
@@ -10,5 +10,19 @@ test("Initial conditions", () => {
   expect(checkbox).not.toBeChecked();
 
   const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+  expect(confirmButton).toBeDisabled();
+});
+
+test("Checkbox enables button on first click, disables on second", async () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+
+  await userEvent.click(checkbox);
+  expect(confirmButton).toBeEnabled();
+
+  await userEvent.click(checkbox);
   expect(confirmButton).toBeDisabled();
 });
