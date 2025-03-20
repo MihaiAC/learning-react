bbae2d87819dba1d1f3c9a9c131471436dbbba39
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-03-20 12:36:37 +0200

    Cleanup routes. Add Confirmation component.

--- learning_react/testing_react_vitest/sundaes-on-demand/src/App.tsx
+++ learning_react/testing_react_vitest/sundaes-on-demand/src/App.tsx
@@ -3,24 +3,22 @@ import { OrderDetailsProvider } from "./components/contexts/OrderDetailsContext"
 import OrderEntry from "./components/pages/order/OrderEntry";
 import SummaryForm from "./components/pages/summary/SummaryForm";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Header from "./components/ui/Header";
+import Confirmation from "./components/pages/confirmation/Confirmation";
 
-// TODO: test routes after finishing the course.
+// TODO: Add routes and test them.
+// TODO: Add styling.
 const router = createBrowserRouter([
-  { path: "/", element: <Header>Welcome to Sundaes on demand!</Header> },
-  {
-    path: "/options",
-    element: (
-      <OrderDetailsProvider>
-        <OrderEntry />
-      </OrderDetailsProvider>
-    ),
-  },
+  { path: "/", Component: OrderEntry },
   { path: "/summary", Component: SummaryForm },
+  { path: "/confirmation", Component: Confirmation },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <OrderDetailsProvider>
+      <RouterProvider router={router} />
+    </OrderDetailsProvider>
+  );
 }
 
 export default App;
