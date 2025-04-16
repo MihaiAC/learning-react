8534d378c5c071c21cedb7ccc78bb0344d2ca15b
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-04-16 19:15:50 +0300

    Remove testing_basics.
    Too basic.

--- testing_basics/testing-routes/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routesConfig from "./routes";
-
-export const router = createBrowserRouter(routesConfig);
-
-export default function App() {
-  return <RouterProvider router={router} />;
-}
