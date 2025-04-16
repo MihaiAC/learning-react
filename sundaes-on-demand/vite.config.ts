2e090f98c111fd7f03bd5145f8f060b893cbea15
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-04-16 19:14:16 +0300

    Add coverage report.

--- sundaes-on-demand/vite.config.ts
+++ sundaes-on-demand/vite.config.ts
@@ -16,5 +16,10 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+      reportsDirectory: "./coverage",
+    },
   },
 });
