493cbfbae8340d2d650760fa1ced6e9917db8d9d
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-04-23 20:50:57 +0300

    Initialise new React + Tailwind UI project.

--- Readme.md
+++ Readme.md
@@ -5,6 +5,9 @@
 - [**Sundaes On Demand**](./sundaes-on-demand/)
   Testing-focused React + TS + Tailwind app.
 
+- [**Browser Extensions Manager UI**](./browser_extension_manager_ui/)
+  Frontend Mentor UI challenge with some JS functionality + Tailwind.
+
 - [**React Three Fiber Tutorial**](./three_fiber_tutorial/)
   Crossy Road clone tutorial, with some extra refactoring and gameplay tweaks.
 
---
+++ browser_extension_manager_ui/eslint.config.js
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
+  {
+    plugins: {
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+    },
+  },
+]);
