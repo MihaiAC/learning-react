23ed756e63d9df4a30147f6e78e00f483f557732
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-04-28 21:52:20 +0300

    Add hook for managing theme; make gradient part of the themes.

--- browser_extension_manager_ui/src/App.jsx
+++ browser_extension_manager_ui/src/App.jsx
@@ -3,10 +3,12 @@ import { CardData } from "./data/CardData";
 import { DisplayState } from "./data/DisplayState";
 import data from "./data/data.json";
 import Card from "./components/Card";
+import { useTheme } from "./ui/hooks/useTheme";
 
 function App() {
   const [cards, setCards] = useState([]);
   const [displayState, setDisplayState] = useState(DisplayState.all);
+  const { theme, toggleTheme, isDark } = useTheme();
 
   // Read the card data from data.json.
   useEffect(() => {
@@ -22,7 +24,7 @@ function App() {
   }
 
   return (
-    <div>
+    <div className={"min-h-screen w-full gradient-bg"}>
       <button onClick={() => setDisplayState(DisplayState.all)}>All</button>
       <button onClick={() => setDisplayState(DisplayState.active)}>
         Active
---
+++ browser_extension_manager_ui/src/ui/hooks/useTheme.js
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
+
+export function useTheme() {
+  const [theme, setTheme] = useState("my-dark-theme");
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+
+    if (theme === "my-dark-theme") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  function toggleTheme() {
+    setTheme(theme === "my-light-theme" ? "my-dark-theme" : "my-light-theme");
+  }
+
+  const isDark = theme === "my-dark-theme";
+
+  return { theme, isDark, toggleTheme };
+}
