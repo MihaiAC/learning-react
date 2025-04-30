ed694b18627fb655494341806d54e4133c4a4905
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-04-30 17:18:55 +0300

    Add GET data, fix button hover styling.

--- advice_generator/src/App.tsx
+++ advice_generator/src/App.tsx
@@ -1,14 +1,37 @@
+import { useEffect, useState } from "react";
+
+interface Advice {
+  id: number;
+  advice: string;
+}
+
 function App() {
+  const [adviceData, setAdviceData] = useState<Advice>({
+    id: 0,
+    advice: "Loading advice...",
+  });
+
+  async function getAdvice() {
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const data = await response.json();
+      setAdviceData({ id: data.slip.id, advice: data.slip.advice });
+    } catch (error) {
+      console.error("Failed to fetch advice: ", error);
+    }
+  }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
   return (
     <div className="flex flex-col justify-center items-center text-white w-full h-screen bg-darkGray">
-      <div className="bg-midGray relative p-6 rounded-xl shadow-md mx-auto max-w-100 flex flex-col items-center space-y-8">
+      <div className="bg-midGray relative p-6 rounded-xl shadow-md mx-auto max-w-100 md:max-w-xl flex flex-col items-center space-y-8">
         <div className="text-hospitalGreen uppercase tracking-widest text-xs">
-          Advice #117
+          Advice #{adviceData.id}
         </div>
-        <div className="text-3xl mx-4 text-center">
-          "It is easy to sit up and take notice, what's difficult is getting up
-          and taking action."
-        </div>
+        <div className="text-3xl mx-4 text-center">"{adviceData.advice}"</div>
         <img
           className="hidden md:block"
           src="/images/pattern-divider-desktop.svg"
@@ -19,9 +42,9 @@ function App() {
           src="/images/pattern-divider-desktop.svg"
           alt=""
         />
-        <div className="absolute -bottom-6 bg-hospitalGreen rounded-full p-4 shadow-md">
+        <button onClick={getAdvice} className="absolute -bottom-6 dice-button">
           <img src="/images/icon-dice.svg" alt="" />
-        </div>
+        </button>
       </div>
     </div>
   );
