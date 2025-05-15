2bf32e14183bd67b55fc9db3f0629da74e0a9b95
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-05-15 18:45:13 +0300

    Fix eslint config, add "vite-plugin-svgr".

--- simple-gallery/eslint.config.js
+++ simple-gallery/eslint.config.js
@@ -1,24 +1,33 @@
+import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import reactPlugin from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    languageOptions: { globals: globals.browser },
-  },
-  pluginReact.configs.flat["jsx-runtime"],
-  {
-    plugins: {
-      "react-hooks": reactHooks,
+    files: ["**/*.{js,jsx,mjs,cjs}"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+      globals: { ...globals.browser },
     },
+    plugins: { js },
+    extends: ["plugin:js/recommended"],
+  },
+
+  {
+    files: ["**/*.{js,jsx}"],
+    ...reactPlugin.configs.flat.recommended,
+    ...reactPlugin.configs.flat["jsx-runtime"],
+  },
+
+  {
+    files: ["**/*.{js,jsx}"],
+    plugins: { "react-hooks": reactHooks },
     rules: {
       ...reactHooks.configs.recommended.rules,
     },
